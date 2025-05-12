82bfcdd31b7f5da66fb736b3d7eaf2cbdce7596a
MeghanaSubramanyam <152699209+MeghanaSubramanyam@users.noreply.github.com>
2025-05-12 22:03:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,1 @@
-var isDate = function (input) {
-  if (input instanceof Date && !isNaN(input.getTime())) return true;
-  const date = new Date(input);
-  return !isNaN(date.getTime());
-};
 
-// Always convert to string for alert
-const input = prompt("Enter Date.");
-alert(String(isDate(input)));
